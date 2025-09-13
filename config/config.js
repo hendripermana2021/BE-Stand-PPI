aa1c9963816de8e6affc650da2bff2628d30675a
Hendri Permana Putra <82196650+hendripermana2021@users.noreply.github.com>
2025-09-13 01:43:21 +0000

    update env

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ export default {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     dialect: "postgres"
   },
@@ -13,6 +14,7 @@ export default {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     dialect: "postgres"
   }
