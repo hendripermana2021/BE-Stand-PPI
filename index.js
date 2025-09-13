5124a32621b23750a212ee0255c7c16e77552709
Hendri Permana Putra <82196650+hendripermana2021@users.noreply.github.com>
2025-09-13 02:44:18 +0000

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import UsersRoute from './route/UsersRoute.js'
+import cors from 'cors';
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import StuffRoute from './route/StuffRoute.js'
@@ -7,6 +8,7 @@ import EntryRoute from './route/EntryRoute.js'
 const app = express()
 const port = process.env.PORT || 3000; // fallback ke 3000 kalau dijalankan local
 
+app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser()); // Kalau pakai cookie
